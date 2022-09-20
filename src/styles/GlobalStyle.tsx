096fa81bb11fc514cb18c1228f47e790f5c86db8
Xifre Font <xifre.font.97@gmail.com>
2022-09-20 13:15:06 +0200

    CSS reset and defined main variables

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,8 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
-    margin: 0
+    margin: 0;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  button, input {
+    font: inherit;
   }
 `;
 
